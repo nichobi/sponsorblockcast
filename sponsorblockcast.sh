e97b4a5ff8e358a6fdf5fae0399f3029daa2512f
iMacThere4iAm <iMacThere4iAm@gmail.com>
2021-10-17 07:30:49 +0100

    Add comments

--- sponsorblockcast.sh
+++ sponsorblockcast.sh
@@ -12,10 +12,12 @@ categories="[$(echo "$SBCCATEGORIES" | sed 's/[^ ]\+/"&"/g;s/\s/,/g')]"
 trap "exit" INT TERM
 trap "kill 0" EXIT
 
+# Create and cleanup temporary directory
 [ -e "$SBCDIR" ] && rm -r "$SBCDIR"
 mkdir "$SBCDIR" || exit 1
 cd    "$SBCDIR" || exit 1
 
+# Download skippable segments data from SponsorBlock
 get_segments () {
   id=$1
   if [ -n "$id" ] && [ ! -f "$id".json ]
@@ -24,6 +26,7 @@ get_segments () {
   fi
 }
 
+# Test if the input has changed since the last time this function was called
 has_variable_changed () {
   if [ "$*" = "$prev_value" ] ; then
     return 1 # not changed
@@ -33,6 +36,7 @@ has_variable_changed () {
   fi
 }
 
+# Fallback search method in case the Chromecast device fails to pass the videoId to go-chromecast
 get_videoID_by_API () {
   if [ -n "$SBCYOUTUBEAPIKEY" ] ; then
     curl -fs --get "https://www.googleapis.com/youtube/v3/search" --data-urlencode "q=\"$video_artist\" \"intitle:\"$video_title\"" --data-urlencode "maxResults=1" --data-urlencode "key=$SBCYOUTUBEAPIKEY" \
@@ -56,6 +60,7 @@ watch () {
       if [ -z "$video_id" ]
       then
         if has_variable_changed "$video_title $video_artist"
+          # Avoid repeating the API search unless the playing video has changed.
         then
           video_id="$(get_videoID_by_API)" && prev_video_id="$video_id"
         else
@@ -63,7 +68,7 @@ watch () {
         fi
       fi
 
-      if [ -n "$video_id" ]; then
+      if [ -n "$video_id" ]; then # Only try to continue if the video has been identified successfully
         get_segments "$video_id"
         progress=$(echo "$status" | grep -oP 'remaining=\K[^s]+')
         jq --raw-output '.[] | (.segment|map_values(tostring)|join(" ")) + " " + .category' "$video_id".json 2> /dev/null |\
@@ -97,12 +102,12 @@ while :
 do
   scan_chromecasts
   while read -r uuid; do
-    uuid_var="sbc$uuid"
-    if [ -z "$(expand "$uuid_var")" ] || ! pid_exists "$(expand "$uuid_var")"
+    uuid_var="sbc$uuid" # Create a dynamically named pid variable for each $uuid
+    if [ -z "$(expand "$uuid_var")" ] || ! pid_exists "$(expand "$uuid_var")" # If the pid variable is empty or the pid does not exist; prevents duplicate watch subprocesses
     then
-      watch "$uuid" &
-      eval "$uuid_var=$!"
-      echo watching "$uuid", pid="$(expand "$uuid_var")"
+      watch "$uuid" & # Watch in the background
+      eval "$uuid_var=$!" # Save the pid into the variable for each $uuid
+      echo watching "$uuid", pid="$(expand "$uuid_var")" # Log
     fi
   done < devices
   sleep "$SBCSCANINTERVAL"
