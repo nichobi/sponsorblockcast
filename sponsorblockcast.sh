ff5c1f1dd44c0a6a4cb8b545eb5c278f861a4c4d
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2022-05-05 08:23:33 +0200

    Improve reliability of and operator, see SC2166
    https://www.shellcheck.net/wiki/SC2166

--- sponsorblockcast.sh
+++ sponsorblockcast.sh
@@ -91,7 +91,7 @@ watch () {
     fi
 
     supported_cmd=$(echo "$status" | grep -oP "\"supportedMediaCommands\":\K[0-9]+")
-    if [ -n "$supported_cmd" -a $(( supported_cmd & 0x2 )) -eq $(( 0x0 )) ]; then
+    if [ -n "$supported_cmd" ] && [ $(( supported_cmd & 0x2 )) -eq $(( 0x0 )) ]; then
       #Ad is skippable
       echo "Skipping skippable ad"
       go-chromecast -u "$uuid" skipad
